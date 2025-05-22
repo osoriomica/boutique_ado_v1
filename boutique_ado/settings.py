f33ef420af3007ae8e679f6d73a25f003c0ff706
Michelle Osorio <osoriomica@gmail.com>
2025-05-22 16:48:47 +0100

    add home app and templates

--- boutique_ado/settings.py
+++ boutique_ado/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
     'allauth',
     'allauth.account',
     'allauth.socialaccount',
+    'home',  # Your custom app
 ]
 
 MIDDLEWARE = [
@@ -59,7 +60,10 @@ ROOT_URLCONF = 'boutique_ado.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            os.path.join(BASE_DIR, 'templates'),  # Custom templates directory
+            os.path.join(BASE_DIR, 'templates', 'allauth'),  # Allauth templates
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
--- boutique_ado/urls.py
+++ boutique_ado/urls.py
@@ -20,4 +20,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('allauth.urls')),  # Include allauth URLs for authentication
+    path('', include('home.urls')),  # Include home app URLs
 ]
