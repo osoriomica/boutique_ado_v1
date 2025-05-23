665f09b4c70f18948ab39468fd1461d43ad03abc
Michelle Osorio <osoriomica@gmail.com>
2025-05-23 22:21:29 +0100

    Add products views and templates

--- boutique_ado/urls.py
+++ boutique_ado/urls.py
@@ -23,4 +23,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('allauth.urls')),  # Include allauth URLs for authentication
     path('', include('home.urls')),  # Include home app URLs
+    path('products/', include('products.urls'))
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
