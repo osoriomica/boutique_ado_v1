f97d75a06e39b0a0b5ac66f56ef415b49c08eeca
Michelle Osorio <osoriomica@gmail.com>
2025-05-24 21:37:33 +0100

    Add quantity input +/- buttons

--- bag/contexts.py
+++ bag/contexts.py
@@ -11,6 +11,7 @@ def bag_contents(request):
     bag = request.session.get('bag', {})
 
     for item_id, item_data in bag.items():
+
         if isinstance(item_data, int):
             product = get_object_or_404(Product, pk=item_id)
             total += item_data * product.price
@@ -20,6 +21,7 @@ def bag_contents(request):
                 'quantity': item_data,
                 'product': product,
             })
+
         else:
             product = get_object_or_404(Product, pk=item_id)
             for size, quantity in item_data['items_by_size'].items():
