8b14d20339da1ca547308543214cea3f879166f2
Michelle Osorio <osoriomica@gmail.com>
2025-05-29 16:49:56 +0100

    Add checkout views and template

--- boutique_ado/urls.py
+++ boutique_ado/urls.py
@@ -24,5 +24,6 @@ urlpatterns = [
     path('accounts/', include('allauth.urls')),  # Include allauth URLs for authentication
     path('', include('home.urls')),  # Include home app URLs
     path('products/', include('products.urls')),
-    path('bag/', include('bag.urls'))
+    path('bag/', include('bag.urls')),
+    path('checkout/', include('checkout.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
