33dfde9988aa3db8a363e465dce873bb60bc408b
Michelle Osorio <osoriomica@gmail.com>
2025-05-31 15:14:45 +0100

    Install stripe-cli and set up wh

--- boutique_ado/settings.py
+++ boutique_ado/settings.py
@@ -28,7 +28,10 @@ SECRET_KEY = 'django-insecure-w=qs)fi#@som4-*-#b*kje^hic*h=jy=l6zh-angry*%i)%vvl
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    '127.0.0.1', #vs code preview
+    'localhost', #listen for stripe webhooks
+    ]
 
 
 # Application definition
@@ -178,3 +181,4 @@ STANDARD_DELIVERY_PERCENTAGE = 10
 STRIPE_CURRENCY = 'usd'
 STRIPE_PUBLIC_KEY = os.getenv('STRIPE_PUBLIC_KEY', '')
 STRIPE_SECRET_KEY = os.getenv('STRIPE_SECRET_KEY', '')
+STRIPE_WH_SECRET = os.getenv('STRIPE_WH_SECRET', '')
