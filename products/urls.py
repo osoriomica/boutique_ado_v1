0fa7431b4333862110c45ab8c27b88322a47d7f5
Michelle Osorio <osoriomica@gmail.com>
2025-06-01 16:06:16 +0100

    Product Admin - Deleting Products

--- products/urls.py
+++ products/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('<int:product_id>/', views.product_detail, name='product_detail'),
     path('add/', views.add_product, name='add_product'), # add products
     path('edit/<int:product_id>/', views.edit_product, name='edit_product'), # edit products
+    path('delete/<int:product_id>/', views.delete_product, name='delete_product'), # delete products
 ]
