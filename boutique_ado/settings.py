b9b94270d5ee20b20f9c2d6b0b11e46babf08982
Michelle Osorio <osoriomica@gmail.com>
2025-06-01 20:00:17 +0100

    Fix: add correct aws bucket name

--- boutique_ado/settings.py
+++ boutique_ado/settings.py
@@ -190,7 +190,7 @@ if 'USE_AWS' in os.environ:
     }
 
     # Bucket Config
-    AWS_STORAGE_BUCKET_NAME = 'boutque-ado-vscode-bucket' # change this to your AWS bucket name
+    AWS_STORAGE_BUCKET_NAME = 'boutiqueado-mich' 
     AWS_S3_REGION_NAME = 'us-east-1'
     AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
     AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
